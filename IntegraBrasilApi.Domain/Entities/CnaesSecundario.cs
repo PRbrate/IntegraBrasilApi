ac339380b4e239fb110b0cd48407a3b74a8bb1f5
Paulo <pauloteles08@hotmail.com>
2024-04-01 10:38:29 -0300

    f

---
+++ IntegraBrasilApi.Domain/Entities/CnaesSecundario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IntegraBrasilApi.Entities
+{
+    public class CnaesSecundario
+    {
+        [JsonPropertyName("codigo")]
+        public int Codigo { get; set; }
+
+        [JsonPropertyName("descricao")]
+        public string? Descricao { get; set; }
+    }
+}
